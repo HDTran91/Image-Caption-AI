2a114cd377619359e978fe80ac05711c2fd25790
HDTran91 <khoaiga0103@gmail.com>
2025-08-07 04:16:01 -0500

    completed

--- api/index.py
+++ api/index.py
@@ -1,3 +1,4 @@
+from fastapi import FastAPI
 import gradio as gr
 from PIL import Image
 from transformers import BlipProcessor, BlipForConditionalGeneration
@@ -6,18 +7,24 @@ from transformers import BlipProcessor, BlipForConditionalGeneration
 processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-base")
 model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-base")
 
-def generate_caption(image: Image) -> str:
+def generate_caption(image: Image.Image) -> str:
     inputs = processor(image, return_tensors="pt")
     outputs = model.generate(**inputs)
     caption = processor.decode(outputs[0], skip_special_tokens=True)
     return caption
 
 # Define Gradio Interface
-image = gr.Image(label="Upload Image")
-caption = gr.Textbox(label="Generated Caption")
-gradio_interface = gr.Interface(
+gradio_app = gr.Interface(
     fn=generate_caption,
-    inputs=image,
-    outputs=caption,
+    inputs=gr.Image(label="Upload Image", type="pil"),
+    outputs=gr.Textbox(label="Generated Caption"),
     title="AI Image Captioning"
 ).launch(share=True)
+
+# Create FastAPI app
+app = FastAPI()
+
+# Mount Gradio inside FastAPI
+app = gr.mount_gradio_app(app, gradio_app, path="/")
+
+
